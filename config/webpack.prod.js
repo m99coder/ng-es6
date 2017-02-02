38025793a069998eec8306a7c45f5d8b48cb899b
Marco Lehmann <marco.lehmann@zalando.de>
2017-02-02 18:18:51 +0100

    Added Webpack configuration files

---
+++ config/webpack.prod.js
@@ -0,0 +1,11 @@
+var path = require('path');
+var webpack = require('webpack');
+var config = require('./webpack.config');
+
+config.output = {
+	filename: '[name].bundle.js',
+	publicPath: '/',
+	path: path.resolve(__dirname, '../dist')
+};
+
+module.exports = config;
