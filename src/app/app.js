699539c31ef9a6c8dc45252ddfed6cb984893f71
Marco Lehmann <marco.lehmann@zalando.de>
2017-02-02 19:51:56 +0100

    Added home component and common module

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,13 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import Common from './common/common';
+import Components from './components/components';
 import AppComponent from './app.component';
 
 angular.module('app', [
-	uiRouter
+	uiRouter,
+	Common,
+	Components
 ])
 	.config($locationProvider => {
 		"ngInject";
