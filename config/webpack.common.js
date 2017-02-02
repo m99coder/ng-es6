9629a3b5296ffaa83e9020e8af7131eb6610ac7c
Marco Lehmann <marco.lehmann@zalando.de>
2017-02-02 20:19:28 +0100

    Changed format of webpack loaders

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -6,25 +6,10 @@ module.exports = {
 	entry: {},
 	module: {
 		loaders: [
-			{
-				test: /\.js$/,
-				exclude: [
-					/node_modules/,
-				],
-				loader: 'ng-annotate!babel'
-			},
-			{
-				test: /\.html$/,
-				loader: 'raw'
-			},
-			{
-				test: /\.scss$/,
-				loader: 'style!css!sass'
-			},
-			{
-				test: /\.css$/,
-				loader: 'style!css'
-			}
+			{test: /\.js$/,	exclude: [/node_modules/], loader: 'ng-annotate!babel'},
+			{test: /\.html$/, loader: 'raw'},
+			{test: /\.scss$/, loader: 'style!css!sass'},
+			{test: /\.css$/, loader: 'style!css'}
 		]
 	},
 	plugins: [
