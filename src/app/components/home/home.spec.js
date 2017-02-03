505b26014a52290f9cc63393184c445c54dbb124
Marco Lehmann <marco.lehmann@zalando.de>
2017-02-03 10:37:10 +0100

    Added more useful unit tests

--- src/app/components/home/home.spec.js
+++ src/app/components/home/home.spec.js
@@ -5,8 +5,52 @@ describe('Home', () => {
 
 	beforeEach(window.module(HomeModule));
 
-	it('should run', () => {
-		expect(true).toBeTruthy();
+	beforeEach(inject(($injector) => {
+		$rootScope = $injector.get('$rootScope');
+		$componentController = $injector.get('$componentController');
+		$state = $injector.get('$state');
+		$location = $injector.get('$location');
+		$compile = $injector.get('$compile');
+	}));
+
+	describe('Module', () => {
+
+		it('default component should be home', () => {
+			$location.url('/');
+			$rootScope.$digest();
+			expect($state.current.component).toBe('home');
+		});
+
+	});
+
+	describe('Controller', () => {
+		let controller;
+
+		beforeEach(() => {
+			controller = $componentController('home', {
+				$scope: $rootScope.$new()
+			});
+		});
+
+		it('has a name property', () => {
+			expect(controller.name).toBeDefined();
+		});
+
+	});
+
+	describe('View', () => {
+		let scope, template;
+
+		beforeEach(() => {
+			scope = $rootScope.$new();
+			template = $compile('<home></home>')(scope);
+			scope.$apply();
+		});
+
+		it('has name in template', () => {
+			expect(template.find('h1').html()).toBe('home.html');
+		});
+
 	});
 
 });
